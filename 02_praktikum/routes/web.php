c41c9c8a88b4252f11257594fc67ee8fad8fe030
SriKynanti <kynanti.ti@gmail.com>
2022-09-06 18:15:59 +0700

    Praktiukm 2

--- 02_praktikum/routes/web.php
+++ 02_praktikum/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
