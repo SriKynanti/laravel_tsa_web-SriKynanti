cad5e85d7d0941fe96981a6b35800db8e99e2732
SriKynanti <kynanti.ti@gmail.com>
2022-09-22 23:32:20 +0700

    Fitur Logout

--- pertemuan35/app/Http/Controllers/Api/AuthController.php
+++ pertemuan35/app/Http/Controllers/Api/AuthController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginRequest;
-use App\Http\Requests\RegisterRequest;
-use App\Models\User;
 use App\Traits\ApiResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use ApiResponse;
 
-    public function register(RegisterRequest $request){
+    public function register(RegisterRequest $request)
+    {
         $validated = $request->validated();
         $user = User::create([
             'name' => $validated['name'],
@@ -24,7 +26,7 @@ class AuthController extends Controller
             'password' => Hash::make($validated['password']),
         ]);
 
-        $token = $user->creatToken('auth_token')->plainTextToken;
+        $token = $user->createToken('auth_token')->plainTextToken;
         return $this->apiSuccess([
             'token' => $token,
             'token_type' => 'Bearer',
@@ -48,5 +50,18 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
             'user' => $user,
         ]);
+    }
+
+    public function logout()
+    {
+        try {
+            auth()->user()->tokens()->delete();
+            return $this->apiSuccess('Tokens revoked');
+        } catch (\Throwable $e) {
+            throw new HttpResponseException($this->apiError(
+                null,
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+            ));
+        }
     }
 }
